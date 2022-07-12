d1531b5b79da8969c62474b3af607f1117f7eb9a
walidelmarassy <33372921+walidelmarassy@users.noreply.github.com>
2022-07-12 19:38:29 +0200

    Update dialogLine.java

--- src/main/java/com/invoice/view/dialogLine.java
+++ src/main/java/com/invoice/view/dialogLine.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.invoice.view;
 
 import java.awt.GridLayout;
@@ -10,10 +7,7 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-/**
- *
- * @author aboba
- */
+
 public class dialogLine extends JDialog{
     private JTextField itemNameF;
     private JTextField itemCountF;
